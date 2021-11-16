d46875c98bd6d18397ad704a0721aafa56271388
ChrisArson <85616944+ChrisArson@users.noreply.github.com>
2021-11-16 22:45:21 +0100

    Update pluto.sh

--- pluto.sh
+++ pluto.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
+#zapisanie obecnej daty w zmiennej data
 data=$(date '+%Y-%m-%d %H:%M:%S')
+#wpisanie zmiennej data do pliku info.log
 echo Data stworzenia: ${data} > /logi/info.log
+#zapisanie ilosci uzytych bitow pamieci do zmiennej pamiec
 pamiec=$(cat /sys/fs/cgroup/memory/memory.usage_in_bytes)
+#dodanie jako kolejnej linijki do pliku info.log ilosc uzytych bajtow
 echo Dostepna pamiec: ${pamiec} bajtow >> /logi/info.log
